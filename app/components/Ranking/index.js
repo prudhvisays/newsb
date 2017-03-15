4fd555484d107fed313ab92bc920dd810b24c319
Prudhvi Dharmavarapu <akira.prudhvi@gmail.com>
2017-03-15 12:57:35 +0530

    area charts removed

--- app/components/Ranking/index.js
+++ app/components/Ranking/index.js
@@ -25,7 +25,7 @@ export default class Target extends React.Component { //eslint-disable-line
                 </div>
               {!compressed && <div className="all-50">
                 <div className="custom-chart">
-                  // <AreaChart data={hourData} color={'rgb(81, 212, 255)'} />
+                  
                 </div>
               </div>}
             </div>
@@ -37,7 +37,7 @@ export default class Target extends React.Component { //eslint-disable-line
             </div>
             {!compressed && <div className="all-50">
               <div className="second-chart">
-                // <AreaChart data={weekData} color={'rgb(81, 212, 255)'} />
+                
               </div>
             </div>}
           </div>
