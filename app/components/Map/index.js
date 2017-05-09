69e1c8981d1a1b6180106507d0b39314f0334c6b
Prudhvi Dharmavarapu <akira.prudhvi@gmail.com>
2017-05-10 03:14:05 +0530

    franchise polygon map

--- app/components/Map/index.js
+++ app/components/Map/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Map, Marker, Popup, TileLayer, Path, Polyline } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer, Path, Polygon } from 'react-leaflet';
 import FullscreenControl from 'react-leaflet-fullscreen';
 import ExtendedMarker from './ExtendedMarker';
+import { userRoleType, session } from '../../Api/ApiConstants';
 
 import auth from '../../Api/Auth';
 import MapStyle from './MapStyle';
@@ -42,7 +43,8 @@ export default class PathHistory extends React.Component { // eslint-disable-lin
 
     getBounds() {
         if(this.pathMap) {
-            return this.pathMap.leafletElement.getBounds
+
+          return this.pathMap.leafletElement.getBounds();
         }
     }
     render() {
@@ -74,10 +76,26 @@ export default class PathHistory extends React.Component { // eslint-disable-lin
                     </div>
                   </Popup>
                 </Marker>)
-            }) : null
-        return (
+            }) : null;
+
+        let polybounds = [[[]]];
+       userRoleType() === 'isFranchise'  && session().manager.franchise['geo_fence']['coordinates'][0][0].length > 0 &&
+       session().manager.franchise['geo_fence']['coordinates'][0][0].map((line) => polybounds[0][0].push(line.reverse()))
+
+      const multi_polygon = userRoleType() === 'isFranchise' && session().manager.franchise['geo_fence']['coordinates'].length > 0 ?
+        <Polygon  ref={(map) => { this.pathMap = map; }} color="lime" positions={polybounds[0][0].length>0 ? polybounds : [[[[83.30845177173616, 17.68687648220767], [83.23051750659944, 17.672156637595997]]]]} /> : null;
+
+     const franchiseMarker =  userRoleType() === 'isFranchise'  && session().manager.franchise ? <Marker position={session().manager.franchise.location && session().manager.franchise.location.coordinates[0] ? session().manager.franchise.location.coordinates.reverse() : [0,0]}>
+                        <Popup>
+                          <div className="ink-flex vertical">
+                            <div>ME</div>
+                          </div>
+                        </Popup>
+                      </Marker> : null
+       return (
             <MapStyle
-                ref={(map) => { this.pathMap = map; }}
+                animate={true}
+                maxBounds={userRoleType() === 'isFranchise' ? this.getBounds() : [[85, -180],[-85, 180]]}
                 center={[this.state.lat, this.state.lng]}
                 zoom={this.state.zoom}
                 zoomControl={false}
@@ -88,6 +106,8 @@ export default class PathHistory extends React.Component { // eslint-disable-lin
                   url='https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHJ1ZGh2aXNheXMiLCJhIjoiY2l4aWxnM2xoMDAxMzJ3bzB2ajlpbzJ2eCJ9.L4CdTG9cSB-ADVYQXbH-hw'
               />
                 {pilotMarkers}
+              {multi_polygon}
+              {franchiseMarker}
                 <FullscreenControl position="topright" />
             </MapStyle>
         );
